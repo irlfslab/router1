2e0d403f108e59f572de345d98756c50bb19e133
IL <53288945+irlfslab@users.noreply.github.com>
2025-03-23 00:49:37 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import Logo from "../assets/logo.png";
+
 export const Header = () => {
     return (
-        <div>Header</div>
+        <Header>
+            <a href="/" className="logo">
+                <img src={Logo} alt="Router1 Logo" />
+                <span>Router1</span>
+            </a>
+            <nav className="navigation">
+                <a href="/" className="link">Home</a>
+                <a href="/products" className="link">Products</a>
+                <a href="/contact" className="link">Contact</a>
+            </nav>
+        </Header>
     )
 }
