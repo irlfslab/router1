4b9a892a0c6f54cd7811b54152af4f78e029e3ad
IL <53288945+irlfslab@users.noreply.github.com>
2025-03-25 01:27:38 -0400

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router";
+
 export const Contact = () => {
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+   /*     console.log("------"); */
+        navigate("/");
+    }
+
     return (
-        <div>Contact</div>
+        <>
+            <div className="component">Contact</div>
+            <button onClick={handleSubmit}> Back to Home </button>
+        </>
+        
     )
 }
