a7637c27ea4e9853f9e71f3674ef464f8f7a2498
IL <53288945+irlfslab@users.noreply.github.com>
2025-03-24 23:57:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import { Home } from "./components/Home";
 import { ProductList } from "./components/ProductList";
 import { ProductDetail } from "./components/ProductDetail";
 import { Contact } from "./components/Contact";
+import { Admin } from "./components/Admin";
 import './App.css';
 
 function App() {
+  const user = true;
   return (
     <div className="App">
       <Header />
+      <main>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="products" element={<ProductList />}></Route>
-        <Route path="products/1001" element={<ProductDetail />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="products" element={<ProductList />} />
+        <Route path="products/1001" element={<ProductDetail />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="/admin" element={ user ? <Admin /> : <Navigate to="/" />} />
       </Routes>
+      </main>
       <Footer />
     </div>
   )
