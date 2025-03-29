3b2fc59f77c57b0b0d0a49c1e3d464df2b86678c
IL <53288945+irlfslab@users.noreply.github.com>
2025-03-29 00:12:07 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Home } from "./components/Home";
 import { ProductList } from "./components/ProductList";
 import { ProductDetail } from "./components/ProductDetail";
 import { Contact } from "./components/Contact";
+import { ContactIn } from "./components/ContactIn";
+import { ContactEu } from "./components/ContactEu";
+import { ContactUs } from "./components/ContactUs";
 import { Admin } from "./components/Admin";
 import { PageNotFound } from "./components/PageNotFound";
 import './App.css';
@@ -18,9 +21,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="products" element={<ProductList />} />
         <Route path="products/:id" element={<ProductDetail />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} >
+          <Route path="in" element={<ContactIn />} /> 
+          <Route path="eu" element={<ContactEu />} /> 
+          <Route path="us" element={<ContactUs />} /> 
+        </Route>
         <Route path="/admin" element={ user ? <Admin /> : <PageNotFound />} />
-        <Route path="*" element={<PageNotFound title="404" />} />
+        <Route path="*" element={<PageNotFound title="404"/>} />
       </Routes>
       <Footer />
     </div>
